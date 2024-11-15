2bd31618c93ca2677bb8f4727c8563db86750d04
Aidan Whitman <agwhitman42@tntech.edu>
2024-11-14 16:28:45 -0800

    OOPS

--- lightserver.py
+++ lightserver.py
@@ -28,8 +28,8 @@ def blink_led(duration, num_blinks):
         GPIO.output(LED_PIN, GPIO.LOW)
         time.sleep(duration)
 
-def creaet_header(sequence_number, ack_number, flags):
-    header = struct.pack('!II I', sequence_number, ack, flags)
+def create_header(sequence_number, ack_number, flags):
+    header = struct.pack('!II I', sequence_number, ack_number, flags)
     return header
 
 def handle_client(data, addr, log_location, udp_socket):
